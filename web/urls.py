0a6196db8156a044a9edfc43bfb595bf80287cb9
Yulin Zhu <yulin.zhu@rwth-aachen.de>
2022-06-29 15:22:56 +0200

    p33 用JS来获取用户输入的手机号，前端为form表单字段生成默认id: id_属性名。发送ajax请求。

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from web.views import account
 
 urlpatterns = [
-    url(r'^register/', account.register, name='register'),  # name可以方便反向解析
+    url(r'^register/$', account.register, name='register'),  # name可以方便反向解析
+    url(r'^send/sms/$', account.send_sms, name='send_sms'),  # 反向解析：在前端url里：'{% url "send_sms" %}'
+
 ]
--- web/views/account.py
+++ web/views/account.py
@@ -5,3 +5,8 @@ from web.forms.account import RegisterModelForm
 def register(request):
     form = RegisterModelForm()
     return render(request, 'web/templates/register.html', {'form': form})
+
+def send_sms(request):
+    print(request.GET)
+
+    return HttpResponse('成功')
