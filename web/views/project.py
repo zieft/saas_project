2010eeeabcfe76ee40a3ad354360c3a4f72e4de5
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2022-08-26 20:16:49 +0200

    p85 表单的提交功能实现。模态对话框提交数据必须用ajax。实现了相应的表单验证，并且展示错误信息。整个流程需要熟记。（验证通过功能暂未实现）

--- web/views/project.py
+++ web/views/project.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from web.forms.project import ProjectModelForm
+from django.http import JsonResponse
 
 
 def project_list(request):
     """ 项目列表 """
-    form = ProjectModelForm()
-    return render(request, 'project_list.html', {'form': form})
+    if request.method == 'GET':  # 有网址直接访问，展示项目
+        form = ProjectModelForm()
+        return render(request, 'project_list.html', {'form': form})
 
+    # post请求由添加项目的表单通过ajax提交过来
+    form = ProjectModelForm(data=request.POST)
+    if form.is_valid():
+        pass
+
+    return JsonResponse({'status': False, 'error': form.errors})
