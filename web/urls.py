be6012ea4912eb5f2a1f64bd16ddbffe109236e7
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2022-08-30 17:01:07 +0200

    p11 项目管理页面，概览，问题，统计，文件，文档，设置 6个功能的页面、视图函数、路由的创建，并给相应按钮添加超链接，实现页面正确跳转

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
 # 路由划分
-from django.conf.urls import url
-from web.views import account, home, project
+from django.conf.urls import url, include
+
+from web.views import account, home, project, manage
 
 urlpatterns = [
     url(r'^register/$', account.register, name='register'),  # name可以方便反向解析
@@ -11,10 +12,23 @@ urlpatterns = [
     url(r'^send/sms/$', account.send_sms_fake, name='send_sms'),
     url(r'^index/$', home.index, name='index'),
 
+    # 项目列表
     url(r'^project/list/$', project.project_list, name='project_list'),
     # project_type: {'my', 'join'}
     # eg.: /project/start/join/12/
     url(r'^project/star/(?P<project_type>\w+)/(?P<project_id>\d+)/$', project.project_star, name='project_star'),
     url(r'^project/unstar/(?P<project_type>\w+)/(?P<project_id>\d+)/$', project.project_unstar, name='project_unstar'),
+
+    # 项目管理，涉及非常多的路由，因此再做一次路由分发
+    # 阅读include()源码，内部参数可以是str（路径），也可以是列表中套这url，跟前面的url做拼接
+
+    url(r'^manage/(?P<project_id>\d+)/', include([
+        url(r'^dashboard/$', manage.dashboard, name='dashboard'),
+        url(r'^issues/$', manage.issues, name='issues'),
+        url(r'^statistics/$', manage.statistics, name='statistics'),
+        url(r'^file/$', manage.file, name='file'),
+        url(r'^wiki/$', manage.wiki, name='wiki'),
+        url(r'^setting/$', manage.setting, name='setting'),
+    ])),
 
 ]
