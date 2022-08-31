9fe71176f89c1b8d6b3f30d46741afbb102ed9f0
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2022-08-31 17:17:00 +0200

    p14 用yinclusiontag重写显示菜单

--- web/templatetags/project.py
+++ web/templatetags/project.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.urls import reverse
 
 from web import models
 
@@ -20,3 +21,16 @@ def all_project_list(request):  # 这不是视图函数，所以request参数需
 
     return {'my': my_project_list, 'join': join_project_list}
     # 这里返回的内容可以直接传到上面的html页面
+
+
+@register.inclusion_tag('inclusion/manage_menu_tag.html')
+def manage_menu_list(request):
+    data_list = [
+        {'title': '概览', 'url': reverse('dashboard', kwargs={'project_id': request.tracer.project.id})},
+        {'title': '问题', 'url': reverse('issues', kwargs={'project_id': request.tracer.project.id})},
+        {'title': '统计', 'url': reverse('statistics', kwargs={'project_id': request.tracer.project.id})},
+        {'title': '文档', 'url': reverse('wiki', kwargs={'project_id': request.tracer.project.id})},
+        {'title': '文件', 'url': reverse('file', kwargs={'project_id': request.tracer.project.id})},
+        {'title': '设置', 'url': reverse('setting', kwargs={'project_id': request.tracer.project.id})},
+    ]
+    return {'data_list': data_list}
