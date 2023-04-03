27d443a1f0b146f429befcd617ee91f7e7072b55
Yulin Zhu <zieft@hotmail.com>
2023-04-03 14:15:22 +0200

    p3 前端实现添加问题到数据库，并展示错误信息

--- web/views/issues.py
+++ web/views/issues.py
@@ -1,8 +1,21 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from web.forms.issues import IssuesModelForm
 
 
 def issues(request, project_id):
-    form = IssuesModelForm(request)
-    return render(request, 'issues.html', {'form': form})
+    if request.method == 'GET':
+        form = IssuesModelForm(request)
+        return render(request, 'issues.html', {'form': form})
+
+    print(request.POST)
+    form = IssuesModelForm(request, data=request.POST)
+    if form.is_valid():
+        form.instance.project = request.tracer.project
+        form.instance.creator = request.tracer.user
+        # 添加问题
+        form.save()
+        return JsonResponse({'status': True})
+
+    return JsonResponse({'status': False, 'error': form.errors})
