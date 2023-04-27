271200df51c49118c5674f268ce6bc1347c1c5a7
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2023-04-27 21:24:04 +0200

    p7 前端展示问题列表的实现

--- web/views/issues.py
+++ web/views/issues.py
@@ -1,13 +1,15 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from web import models
 from web.forms.issues import IssuesModelForm
 
 
 def issues(request, project_id):
     if request.method == 'GET':
         form = IssuesModelForm(request)
-        return render(request, 'issues.html', {'form': form})
+        issues_object_list = models.Issues.objects.filter(project_id=project_id)
+        return render(request, 'issues.html', {'form': form, 'issues_object_list': issues_object_list})
 
     print(request.POST)
     form = IssuesModelForm(request, data=request.POST)
